98caedaa446a0b34fb5f955019dc9e378b5afc9b
atanasbondzhakov <atanas.b@yahoo.com>
2025-04-08 09:18:50 +0300

    Create EditModal component

---
+++ todo-list/src/components/EditModal.jsx
@@ -0,0 +1,14 @@
+export default function EditModal() {
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                <h2>Edit Todo</h2>
+                <input
+                    type="text"
+                />
+                <button>Update</button>
+                <button>Cancel</button>
+            </div>
+        </div>
+    );
+};
